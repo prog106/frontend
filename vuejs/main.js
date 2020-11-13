1388a0fcf12f5b85c4dcf292ad1a26d6535fa26c
prog106 <prog106@gmail.com>
2020-11-13 19:05:10 +0900

    study

--- vuejs/main.js
+++ vuejs/main.js
@@ -36,7 +36,7 @@ var app4 = new Vue({
             { text: '2번' },
             { text: '3번' },
             { text: '4번' },
-        ]
+        ],
     }
 });
 app4.todos.push({text:'5번'});
@@ -51,5 +51,133 @@ var app5 = new Vue({
         reserveMethod: function() {
             this.message = this.message.split('').reverse().join('');
         }
+    }
+});
+
+var app6 = new Vue({
+    el: '#app6',
+    data: {
+        message: 'hi vue!',
+    }
+});
+
+var test1 = new Vue({
+    el: '#test1',
+    data: {
+        choose: true,
+        btn_name: '바꾸기',
+        pants: '/shopping/img/blue_p.png',
+        skirt: '/shopping/img/blue_s.png',
+    },
+    methods: {
+        chooseMethod: function() {
+            this.choose = !this.choose; // this === test1
+        }
+    },
+});
+
+var test2 = new Vue({
+    el: '#test2',
+    data: {
+        blog_name: '거위의 꿈',
+    },
+    methods: {
+        blog_link() {
+            return 'http://color106.egloos.com/' + '#' + this.blog_name;
+        },
+    }
+});
+
+var test3 = new Vue({
+    el: '#test3',
+    data: {
+        year: 2018,
+    },
+    methods: {
+        plus() {
+            this.year++;
+        },
+        minus() {
+            this.year--;
+        }
+    }
+});
+
+var test4 = new Vue({
+    el: '#test4',
+    data: {
+        text1: 'hi',
+        text2: 'hello',
+    },
+    methods: {
+        submit() {
+            alert('submit!');
+        },
+        keyup1(e) {
+            console.log(e.target.value);
+            this.text = e.target.value;
+        },
+    }
+});
+
+var test5 = new Vue({
+    el: '#test5',
+    data: {
+        number: 1,
+        message: '안녕하세요'
+    },
+    methods: { // 캐싱되지 않음
+        m_reversedMessage() { // 호출 m_reversedMessage()
+            this.message = '헬로우';
+            // return this.message.split('').reverse().join('');
+        }
+    },
+    computed: { // 캐싱됨
+        reversedMessage() { // 호출 reversedMessage
+            return this.message.split('').reverse().join('');
+        }
+    }
+});
+
+var test6 = new Vue({
+    el: '#test6',
+    data: {
+        message: '안녕하세요',
+        updated: 'no',
+    },
+    methods: { // 캐싱되지 않음
+        changeMessage() {
+            this.message = '헬로우';
+        }
+    },
+    computed: { // 캐싱됨
+    },
+    watch: { // data.field 값 변경되는 과정 확인
+        message(newval, oldval) { // 변수와 method 이름이 같아야 됨
+            console.log(newval, oldval);
+            this.updated = 'yes'
+        }
+    }
+});
+
+var test7 = new Vue({
+    el: '#test7',
+    data: {
+        isRed: false,
+        isBold: false,
+        red: 'red',
+        size: '20px',
+    },
+    methods: {
+        update() {
+            this.isRed = !this.isRed;
+            this.isBold = !this.isBold;
+        }
+    },
+    computed: {
+
+    },
+    watch: {
+
     }
 });
