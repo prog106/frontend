03b895e2d50ecfc4984197253138eb3cd1762c86
prog106 <prog106@gmail.com>
2021-02-16 19:08:21 +0900

    coffee machine study

---
+++ shopping/src/coffee.js
@@ -0,0 +1,126 @@
+'use strict';
+
+/* 
+커피머신
+가격 1잔당 300원
+커피 1잔 기준
+- 커피가루 : 15g
+- 프림 : 12g
+- 설탕 : 9g
+- 물 : 180mL
+*/
+
+class Machine {
+    constructor(coffee, cream, sugar, water) {
+        this.coffee_mat = {
+            coffee: 15,
+            cream: 12,
+            sugar: 9,
+            water: 180,
+            coin: 300
+        };
+        this.coffee = coffee;
+        this.cream = cream;
+        this.sugar = sugar;
+        this.water = water;
+        this.coin = 0;
+        this.profit = 0;
+    }
+
+    set coffee(value) {
+        this._coffee = value < this.coffee_mat.coffee ? 0 : value;
+    }
+    set cream(value) {
+        this._cream = value < this.coffee_mat.cream ? 0 : value;
+    }
+    set sugar(value) {
+        this._sugar = value < this.coffee_mat.sugar ? 0 : value;
+    }
+    set water(value) {
+        this._water = value < this.coffee_mat.water ? 0 : value;
+    }
+
+    get coffee() {
+        return this._coffee;
+    }
+    get cream() {
+        return this._cream;
+    }
+    get sugar() {
+        return this._sugar;
+    }
+    get water() {
+        return this._water;
+    }
+
+    delay(ms) {
+        return new Promise((resolve, reject) => {
+            setTimeout(resolve, ms);
+        });
+    }
+
+    dashboard() {
+        console.log(`커피 : ${this.coffee} g
+프림 : ${this.cream} g
+설탕 : ${this.sugar} g
+물 : ${this.water} mL
+잔액 : ${this.coin} 원
+이익 : ${this.profit} 원`);
+    }
+
+    insert_coin(coin) {
+        console.warn(coin + '원 입금되었습니다.');
+        this.coin += coin;
+    }
+    remain_coin() {
+        console.warn(this.coin + '원 반환되었습니다.');
+        this.coin = 0;
+    }
+
+    request_coffee() {
+        if(!this.coffee || !this.cream || !this.sugar || !this.water) {
+            console.error('커피 재료가 부족합니다.');
+            this.dashboard();
+            return false;
+        }
+        if(this.coin < this.coffee_mat.coin) {
+            console.error('잔액이 부족합니다.');
+            return false;
+        }
+        return true;
+    }
+    make_coffee() {
+        this.profit += this.coffee_mat.coin;
+        this.coin -= this.coffee_mat.coin;
+        this.coffee -= this.coffee_mat.coffee;
+        this.cream -= this.coffee_mat.cream;
+        this.sugar -= this.coffee_mat.sugar;
+        this.water -= this.coffee_mat.water;
+        console.warn('커피 1잔 나왔습니다.');
+    }
+
+    request(item) {
+        if(item === 'coffee') {
+            if(this.request_coffee()) this.make_coffee();
+        }
+    }
+    charge_coffee_material(coffee, cream, sugar, water) {
+        this.coffee += coffee;
+        this.cream += cream;
+        this.sugar += sugar;
+        this.water += water;
+        console.warn('커피 재료가 보충되었습니다.');
+    }
+}
+
+let machine = new Machine(40, 40, 40, 400);
+
+machine.insert_coin(1000);
+
+machine.request('coffee');
+machine.request('coffee');
+machine.request('coffee');
+
+machine.charge_coffee_material(40, 40, 40, 500);
+machine.request('coffee');
+machine.remain_coin();
