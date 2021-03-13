63d20c6cadf1f1f9cf76473c8de9b8fc7454642a
이수권 <prog106@gmail.com>
2021-03-13 21:14:57 +0900

    channel code

--- nodejs/modules/chat.js
+++ nodejs/modules/chat.js
@@ -5,23 +5,31 @@ moment.locale('ko');
 module.exports = function(io) {
     let chat = io.of('/chat').on('connection', function(socket) {
         let user = socket.request.session.passport.user; // 사용자 세션정보
+        function makeRandomName() { // unique
+            let name = moment().format('YYMMDD');
+            let possible = "abcdefghijklmnopqrstuvwxyz1234567890";
+            for( let i = 0; i < 4; i++ ) {
+                name += possible.charAt(Math.floor(Math.random() * possible.length));
+            }
+            return name;
+        }
         function reset() {
             // 접속자 정보 업데이트
-            db.query(`SELECT CU.socket_id, CU.channel_idx AS channel, U.user_name, U.user_profile FROM test_channel_user CU
+            db.query(`SELECT CU.socket_id, CU.channel_code AS channel, U.user_name, U.user_profile FROM test_channel_user CU
                 INNER JOIN test_user_social U ON U.user_idx = CU.user_idx`,
                 [], function(err, rows, fields) {
                     chat.emit('user_list', rows);
                 }
             );
             // 채널 정보 업데이트
-            db.query(`SELECT C.channel_idx, C.channel_title, C.channel_created_at, COUNT(CU.user_idx) AS channel_user_count FROM test_channel C
-                LEFT JOIN test_channel_user CU ON CU.channel_idx = C.channel_idx GROUP BY 1`,
+            db.query(`SELECT C.channel_code, C.channel_title, C.channel_created_at, COUNT(CU.user_idx) AS channel_user_count FROM test_channel C
+                LEFT JOIN test_channel_user CU ON CU.channel_code = C.channel_code GROUP BY 1`,
                 [], function(err, rows, fields) {
                     // console.log(rows);
                     rows.forEach(function(v, k) {
                         if(v.channel_user_count < 1) {
                             rows.splice(k, 1);
-                            db.query(`DELETE FROM test_channel WHERE channel_idx = ?`, [v.channel_idx], function(err, rows, fields) { });
+                            db.query(`DELETE FROM test_channel WHERE channel_code = ?`, [v.channel_code], function(err, rows, fields) { });
                         }
                     });
                     chat.emit('channel_list', rows);
@@ -45,30 +53,24 @@ module.exports = function(io) {
         // 참여자 채널 만들기 & 접속
         socket.on('channel', function(data) {
             let title = data.title;
-            db.query(`INSERT INTO test_channel (channel_title, channel_created_at) VALUES (?, NOW())`,
-                [title], function(err, rows, fields) {
-                    console.log(err);
-                    console.log(rows);
-                    let channel_idx = rows.insertId;
-                    console.log(channel_idx);
-                    db.query(`UPDATE test_channel_user SET channel_idx = ? WHERE user_idx = ?`,
-                        [channel_idx, user.user_idx], function(err, rows, fields) {
-                            console.log(err);
-                            console.log(rows);
-                            socket.join(channel_idx);
-                            socket.room = channel_idx;
-                            chat.to(channel_idx).emit('join', { // 나에게 or 전체.
-                                channel: channel_idx,
+            let channel_code = makeRandomName();
+            db.query(`INSERT INTO test_channel (channel_code, channel_title, channel_created_at) VALUES (?, ?, NOW())`,
+                [channel_code, title], function(err, rows, fields) {
+                    db.query(`UPDATE test_channel_user SET channel_code = ? WHERE user_idx = ?`,
+                        [channel_code, user.user_idx], function(err, rows, fields) {
+                            socket.join(channel_code);
+                            chat.to(channel_code).emit('join', {
+                                channel: channel_code,
                                 title: title,
                             });
-                            chat.to(channel_idx).emit('notice', {
+                            chat.to(channel_code).emit('notice', {
                                 msg: user.user_name + ' 님이 입장하였습니다!',
                             });
                             db.query(`SELECT U.user_name FROM test_channel_user CU
                                 INNER JOIN test_user_social U ON U.user_idx = CU.user_idx
-                                WHERE CU.channel_idx = ?`,
-                                [channel_idx], function(err, rows, fields) {
-                                    chat.to(channel_idx).emit('join_list', rows);
+                                WHERE CU.channel_code = ?`,
+                                [channel_code], function(err, rows, fields) {
+                                    chat.to(channel_code).emit('join_list', rows);
                                 }
                             );
                             reset();
@@ -79,60 +81,55 @@ module.exports = function(io) {
         });
         // 참여자 채널 접속
         socket.on('channel_join', function(data) {
-            let channel_idx = data.channel;
-            db.query(`SELECT * FROM test_channel WHERE channel_idx = ?`, [channel_idx], function(err, rows, fields) {
+            let channel_code = data.channel;
+            db.query(`SELECT * FROM test_channel WHERE channel_code = ?`, [channel_code], function(err, rows, fields) {
                 if(rows.length < 1) {
                     socket.emit('redirect', '채널에 참여할 수 없습니다.');
                     reset();
                     return false;
                 }
                 let channel_title = rows[0].channel_title;
-                db.query(`UPDATE test_channel_user SET channel_idx = ? WHERE user_idx = ?`, [channel_idx, user.user_idx], function(err, rows, fields) {
-                    socket.join(channel_idx);
-                    socket.room = channel_idx;
+                db.query(`UPDATE test_channel_user SET channel_code = ? WHERE user_idx = ?`, [channel_code, user.user_idx], function(err, rows, fields) {
+                    socket.join(channel_code);
                     socket.emit('join', { // 나에게
-                        channel: channel_idx,
+                        channel: channel_code,
                         title: channel_title,
                     });
-                    chat.to(channel_idx).emit('notice', {
+                    chat.to(channel_code).emit('notice', {
                         msg: user.user_name + ' 님이 입장하였습니다.',
                     });
                     db.query(`SELECT U.user_name FROM test_channel_user CU
                         INNER JOIN test_user_social U ON U.user_idx = CU.user_idx
-                        WHERE CU.channel_idx = ?`,
-                        [channel_idx], function(err, rows, fields) {
-                            console.log(rows);
-                            chat.to(channel_idx).emit('join_list', rows);
+                        WHERE CU.channel_code = ?`,
+                        [channel_code], function(err, rows, fields) {
+                            chat.to(channel_code).emit('join_list', rows);
                         }
                     );
                     reset();
                 });
-
             });
         });
         socket.on('leave', function(data) {
             db.query(`SELECT * FROM test_channel_user WHERE user_idx = ?`, [user.user_idx], function(err, rows, fields) {
                 if(rows.length > 0) {
-                    console.log(rows);
-                    let channel_idx = rows[0].channel_idx;
-                    console.log(channel_idx);
-                    db.query(`UPDATE test_channel_user SET channel_idx = 0 WHERE user_idx = ?`, [user.user_idx], function(err, rows, fields) {
-                        chat.to(channel_idx).emit('notice', {
+                    let channel_code = rows[0].channel_code;
+                    db.query(`UPDATE test_channel_user SET channel_code = '' WHERE user_idx = ?`, [user.user_idx], function(err, rows, fields) {
+                        chat.to(channel_code).emit('notice', {
                             msg: user.user_name + ' 님이 나갔습니다.',
                         });
                     });
                     db.query(`SELECT U.user_name FROM test_channel_user CU
                         INNER JOIN test_user_social U ON U.user_idx = CU.user_idx
-                        WHERE CU.channel_idx = ?`,
-                        [channel_idx], function(err, rows, fields) {
+                        WHERE CU.channel_code = ?`,
+                        [channel_code], function(err, rows, fields) {
                             if(rows.length < 1) { // 아무도 없으면 방 삭제
-                                db.query(`DELETE FROM test_channel WHERE channel_idx = ?`, [channel_idx], function(err, rows, fields) { })
+                                db.query(`DELETE FROM test_channel WHERE channel_code = ?`, [channel_code], function(err, rows, fields) { })
                             } else {
-                                chat.to(channel_idx).emit('join_list', rows);
+                                chat.to(channel_code).emit('join_list', rows);
                             }
                         }
                     );
-                    socket.leave(channel_idx);
+                    socket.leave(channel_code);
                     socket.room = 0;
                 }
                 reset();
@@ -141,21 +138,21 @@ module.exports = function(io) {
         socket.on('disconnect', function(data) {
             db.query(`SELECT * FROM test_channel_user WHERE user_idx = ?`, [user.user_idx], function(err, rows, fields) {
                 if(rows.length > 0) {
-                    let channel_idx = rows[0].channel_idx;
-                    socket.leave(channel_idx);
-                    db.query(`UPDATE test_channel_user SET channel_idx = 0 WHERE user_idx = ?`, [user.user_idx], function(err, rows, fields) {
-                        chat.to(channel_idx).emit('notice', {
+                    let channel_code = rows[0].channel_code;
+                    socket.leave(channel_code);
+                    db.query(`UPDATE test_channel_user SET channel_code = '' WHERE user_idx = ?`, [user.user_idx], function(err, rows, fields) {
+                        chat.to(channel_code).emit('notice', {
                             msg: user.user_name + ' 님이 나갔습니다.',
                         });
                     });
                     db.query(`SELECT U.user_name FROM test_channel_user CU
                         INNER JOIN test_user_social U ON U.user_idx = CU.user_idx
-                        WHERE CU.channel_idx = ?`,
-                        [channel_idx], function(err, rows, fields) {
+                        WHERE CU.channel_code = ?`,
+                        [channel_code], function(err, rows, fields) {
                             if(rows.length < 1) { // 아무도 없으면 방 삭제
-                                db.query(`DELETE FROM test_channel WHERE channel_idx = ?`, [channel_idx], function(err, rows, fields) { })
+                                db.query(`DELETE FROM test_channel WHERE channel_code = ?`, [channel_code], function(err, rows, fields) { })
                             } else {
-                                chat.to(channel_idx).emit('join_list', rows);
+                                chat.to(channel_code).emit('join_list', rows);
                             }
                         }
                     );
@@ -165,10 +162,10 @@ module.exports = function(io) {
             });
         });
         socket.on('chat', function(data) {
-            let channel_idx = data.channel;
+            let channel_code = data.channel;
             let msg = data.msg;
-            db.query(`INSERT INTO test_channel_chat (channel_idx, user_idx, chat, chat_created_at) VALUES (?, ?, ?, NOW())`,
-                [channel_idx, user.user_idx, msg],
+            db.query(`INSERT INTO test_channel_chat (channel_code, user_idx, chat, chat_created_at) VALUES (?, ?, ?, NOW())`,
+                [channel_code, user.user_idx, msg],
                 function(err, rows, fields) {
                     if(rows.insertId > 0) {
                         let res = {
@@ -177,10 +174,10 @@ module.exports = function(io) {
                             msg: data.msg,
                             time: moment().format('LT'), // 오후 2:48
                         }
-                        chat.to(channel_idx).emit('chat', res);
+                        chat.to(channel_code).emit('chat', res);
                     } else {
                         // 메시지 전송 실패 - 나에게만
-                        chat.to(channel_idx).emit('notice', {
+                        chat.to(channel_code).emit('notice', {
                             idx: user.user_idx,
                             msg: '메시지 발송 실패',
                         });
