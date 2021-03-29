03dae93f7c22d798ec13b6bb5c2dc0a20df3ebe4
prog106 <prog106@gmail.com>
2021-03-29 19:10:06 +0900

    card

---
+++ javascript/game/card.js
@@ -0,0 +1,85 @@
+'use strict';
+
+function Cards() {
+    let hor = 4; // 가로
+    let ver = 3; // 세로
+    let flag_click = true;
+    let open_count = 0;
+    let click_card = [];
+    let btn = document.querySelector('#try');
+    btn.addEventListener('click', function() {
+        card();
+    });
+    btn.click();
+    function shuffle() {
+        let msg = [ '2', '2', '4', '4', '6', '6', '7', '7', '8', '8', '5', '5'];
+        let msg_pos = [];
+        while(msg_pos.length < hor*ver) {
+            msg_pos.push(msg.splice(Math.floor(Math.random() * msg.length), 1)[0]);
+        }
+        return msg_pos;
+    }
+    function card() {
+        btn.disabled = true;
+        flag_click = false;
+        let msg_pos = shuffle();
+        let cards = document.querySelector('.cards');
+        cards.innerHTML = '';
+        for(let i=0; i<hor*ver; i++) {
+            let d = document.createElement('div');
+            d.classList.add('card');
+            let f = document.createElement('div');
+            f.classList.add('card_front');
+            let b = document.createElement('div');
+            b.classList.add('card_back');
+            let h = document.createElement('h1');
+            h.textContent = msg_pos[i];
+            b.appendChild(h);
+            d.appendChild(f);
+            d.appendChild(b);
+            cards.appendChild(d);
+            d.addEventListener('click', function() {
+                if(flag_click && !click_card.includes(d) && !d.classList.contains('flipped')) {
+                    click_card.push(d);
+                    d.classList.add('flipped');
+                    if(click_card.length === 2) {
+                        flag_click = false;
+                        if(click_card[0].childNodes[1].childNodes[0].textContent !== click_card[1].childNodes[1].childNodes[0].textContent) {
+                            setTimeout(function() {
+                                click_card[0].classList.remove('flipped');
+                                click_card[1].classList.remove('flipped');
+                                click_card = [];
+                                flag_click = true;
+                            }, 1000);
+                        } else {
+                            open_count += 2;
+                            if(open_count === hor*ver) {
+                                flag_click = false;
+                                setTimeout(function() {
+                                    alert('성공');
+                                }, 1000);
+                            } else {
+                                flag_click = true;
+                            }
+                            click_card = [];
+                        }
+                    }
+                }
+            });
+        }
+        setTimeout(function() {
+            document.querySelectorAll('.card').forEach(function(item, index) {
+                setTimeout(function() {
+                    item.classList.add('flipped');
+                }, 100*index);
+            });
+        }, 1000);
+        setTimeout(function() {
+            document.querySelectorAll('.card').forEach(function(item, index) {
+                item.classList.remove('flipped');
+                flag_click = true;
+                btn.disabled = false;
+            });
+        }, 4000);
+    }
+}
--- nodejs/public/game/speed/speed.js
+++ nodejs/public/game/speed/speed.js
@@ -4,18 +4,23 @@ let screen = document.querySelector('#screen');
 let start;
 let end;
 let timer;
+let record = [];
 screen.addEventListener('click', function() {
     if(screen.classList.contains('waiting')) {
         screen.classList.remove('waiting');
         screen.classList.add('ready');
         screen.textContent = '초록색이 되면 클릭하세요.';
         timer = setTimeout(function() {
-            screen.click();
             start = new Date();
+            screen.click();
         }, Math.floor((Math.random() * 1000) + 2000));
     } else if(screen.classList.contains('ready')) {
-        if(!start) clearTimeout(timer);
-        else {
+        if(!start) {
+            clearTimeout(timer);
+            screen.classList.remove('ready');
+            screen.classList.add('waiting');
+            screen.textContent = '너무 성급하시군요!';
+        } else {
             screen.classList.remove('ready');
             screen.classList.add('now');
             screen.textContent = '클릭하세요.';
@@ -26,8 +31,8 @@ screen.addEventListener('click', function() {
         screen.textContent = '클릭해서 시작하세요.';
         end = new Date();
         let time = end - start;
+        record.push(time);
         start = null;
         end = null;
-        console.log(time/1000 + ' sec');
     }
 });
