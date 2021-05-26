85c1d4b2da5c3a9820dbeeab008748ee22e63460
prog106 <prog106@gmail.com>
2021-05-26 19:02:36 +0900

    soobooke

--- soobooke/public/js/bookshelf.js
+++ soobooke/public/js/bookshelf.js
@@ -103,8 +103,8 @@ let Bookshelf = function() {
                 <div class="book_status">${icon}</div>
                 <div class="book_isbn">ISBN : <span>${item.isbn13}</span></div>
                 <div class="book_author">저자 : <span>${item.authors.split('|').join(', ')}</span></div>
-                <div class="book_translator">번역 : <span>${item.translators}</span></div>
-                <div class="book_publisher">출판 : <span>${item.publisher}</span></div>
+                <div class="book_translator">번역 : <span>${item.translators.split('|').join(', ')}</span></div>
+                <div class="book_publisher">출판 : <span>${item.publisher.split('|').join(', ')}</span></div>
                 <div class="book_regdate">${item.regdate}</div>
                 <div class="book_link"><a href="${item.link}" target="_blank">책 상세보기</a></div>
                 <div class="book_button">
@@ -113,12 +113,123 @@ let Bookshelf = function() {
             </div>
         </li>`;
     }
+    function bookshelf_modal() {
+        document.querySelector('.bookshelf_add').addEventListener('click', function() {
+            setTimeout(function() {
+                document.querySelector('.modal').style.display = 'block';
+                document.querySelector('.modal .book_search_modal').style.display = 'block';
+                document.querySelector('.wrap').style.display = 'none';
+                document.querySelector('input[name=keyword]').value = '';
+                document.querySelector('input[name=keyword]').focus();
+            }, 200);
+        });
+        document.querySelector('.modal .book_search .close').addEventListener('click', function() {
+            setTimeout(function() {
+                document.querySelector('.modal').style.display = 'none';
+                document.querySelector('.modal .book_search_modal').style.display = 'none';
+                document.querySelector('.wrap').style.display = 'block';
+                document.querySelector('input[name=keyword]').value = '';
+                document.querySelector('.book_search_list').innerHTML = '';
+                document.querySelector('.book_search_nodata').style.display = 'flex';
+                getinfo();
+            }, 200);
+        });
+    }
+    function booksearch(page=1) {
+        if(fetch) return false;
+        fetch = true;
+        // form 데이터 처리
+        let form_data = new FormData(search_book_form);
+        form_data.append('page', page);
+        let url = search_book_form.action;
+        let shtml = '';
+        common.ax_fetch_put(url, form_data, function(res) {
+            fetch = false;
+            if(res.data.length > 0) {
+                if(page == 1) {
+                    book_data = [];
+                    document.querySelector('.book_search_list').innerHTML = '';
+                    shtml += '<li class="book_info">검색 결과 ( ' + res.info.total_count + ' )</li>';
+                }
+                book_data = book_data.concat(res.data);
+                res.data.forEach(function(v, k) {
+                    let thumbnail = v.thumbnail.replace('type=m1&', 'type=m140&');
+                    shtml += `<li class="book_info">
+                        <div class="book_image">
+                            <img src="${thumbnail}" alt="">
+                        </div>
+                        <div class="book_subinfo">
+                            <div class="book_title">${v.title.replace(/<[^>]*>?/gm, '')}</div>
+                            <div class="book_isbn">ISBN : <span>${v.isbn13}</span></div>
+                            <div class="book_author">저자 : <span>${v.authors.split('|').join(', ')}</span></div>
+                            <div class="book_translator">번역 : <span>${v.translators.split('|').join(', ')}</span></div>
+                            <div class="book_publisher">출판 : <span>${v.publisher.split('|').join(', ')}</span></div>
+                            <div class="book_regdate">${v.regdate}</div>
+                            <div class="book_link"><a href="${v.link}" target="_blank">책 소개</a></div>
+                            <div class="book_button">
+                                <button class="add_book" onclick="Commonjs.add_bookshelf(${v.isbn13}, this)">우리 가족 책장에 담기 +</button>
+                            </div>
+                        </div>
+                    </li>`;
+                });
+                document.querySelector('.book_search_nodata').style.display = 'none';
+                page++;
+                if(!res.info.is_end) shtml += `<li class="book_info more" onclick="Bookshelf.booksearch(${page})">더 보기 +</li>`;
+                if(document.querySelector('.book_info.more')) document.querySelector('.book_info.more').remove();
+                document.querySelector('.book_search_list').insertAdjacentHTML('beforeend', shtml);
+                if(document.querySelector('input[name=keyword]:focus')) document.querySelector(':focus').blur();
+            } else {
+                document.querySelector('.book_search_list').insertAdjacentHTML('beforeend', shtml);
+            }
+        });
+    }
+    function add_bookshelf(isbn13, btn) {
+        if(fetch) return false;
+        fetch = true;
+        let book = book_data.find(function(v) { if(v.isbn13 == isbn13) return true; });
+        let url = '/bookshelf';
+        let form_data = new FormData();
+        form_data.append('book', JSON.stringify(book));
+        common.ax_fetch_post(url, form_data, function(res) {
+            fetch = false;
+            if(res.success) {
+                common.notification('우리 가족 책장에 담았습니다.', 20);
+                btn.innerHTML = '우리 가족 책장에 담았습니다.';
+                btn.onclick = null;
+            } else {
+                if(res.code == 'already') {
+                    btn.innerHTML = res.message;
+                    btn.onclick = null;
+                }
+                if(res.message) alert(res.message);
+                if(res.code == 'logout') common.logout();
+                if(res.code == 'choose') common.choose();
+            }
+        });
+    }
+    function booksearchtext() {
+        let remove_btn = document.querySelector('form#search_book_form .remove_keyword');
+        let keyword = document.querySelector('form#search_book_form input[name=keyword]');
+        if(keyword.value) {
+            remove_btn.style.display = 'inline-block';
+        } else {
+            remove_btn.style.display = 'none';
+        }
+    }
+    function deletebooksearchtext() {
+        document.querySelector('form#search_book_form input[name=keyword]').value = '';
+        document.querySelector('form#search_book_form .remove_keyword').style.display = 'none';
+    }
     return {
         init: function() {
             shelfclass();
             bookshelf_search();
+            bookshelf_modal();
         }(),
-        getinfo: getinfo,
         move_myshelf: move_myshelf,
+        booksearchtext: booksearchtext,
+        deletebooksearchtext: deletebooksearchtext,
+        booksearch: booksearch,
+        add_bookshelf: add_bookshelf,
     }
 }();
--- soobooke/public/js/common.js
+++ soobooke/public/js/common.js
@@ -50,10 +50,6 @@ let common = {
             document.querySelector('.dimmed').classList.remove('on');
         }, 200);
     },
-    golnbsearch: function() {
-        common.lnbclose();
-        common.lnbsearch();
-    },
     login: function() {
         window.location.href = '/login';
     },
@@ -140,26 +136,6 @@ let common = {
     gotop: function() {
         window.scrollTo({ top: 0, behavior: 'smooth' });
     },
-    lnbsearch: function() {
-        setTimeout(function() {
-            document.querySelector('.modal').style.display = 'block';
-            document.querySelector('.modal .book_search_modal').style.display = 'block';
-            document.querySelector('.wrap').style.display = 'none';
-            document.querySelector('input[name=keyword]').value = '';
-            document.querySelector('input[name=keyword]').focus();
-        }, 200);
-    },
-    lnbsearchclose: function() {
-        setTimeout(function() {
-            document.querySelector('.modal').style.display = 'none';
-            document.querySelector('.modal .book_search_modal').style.display = 'none';
-            document.querySelector('.wrap').style.display = 'block';
-            document.querySelector('input[name=keyword]').value = '';
-            document.querySelector('.book_search_list').innerHTML = '';
-            document.querySelector('.book_search_nodata').style.display = 'flex';
-            if(window.location.pathname == '/bookshelf' && Bookshelf) Bookshelf.getinfo();
-        }, 200);
-    },
     kakaologin: function() {
         setTimeout(function() {
             let agent = navigator.userAgent.toLowerCase();
@@ -210,16 +186,13 @@ let Commonjs = function() {
             if(window.scrollY > 40) {
                 if(y > window.scrollY) { // 위로 스크롤
                     document.querySelector('.header').classList.remove('slide_up');
-                    document.querySelector('.footer').classList.remove('slide_down');
                     document.querySelector('.gotop').classList.remove('slide_down');
                 } else { // 아래로 스크롤
                     document.querySelector('.header').classList.add('slide_up');
-                    document.querySelector('.footer').classList.add('slide_down');
                     document.querySelector('.gotop').classList.add('slide_down');
                 }
             } else {
                 document.querySelector('.header').classList.remove('slide_up');
-                document.querySelector('.footer').classList.remove('slide_down');
                 document.querySelector('.gotop').classList.remove('slide_down');
             }
             y = window.scrollY;
@@ -227,97 +200,9 @@ let Commonjs = function() {
             else document.querySelector('.gotop').classList.add('on');
         });
     }
-    function booksearchtext() {
-        let remove_btn = document.querySelector('form#search_book_form .remove_keyword');
-        let keyword = document.querySelector('form#search_book_form input[name=keyword]');
-        if(keyword.value) {
-            remove_btn.style.display = 'inline-block';
-        } else {
-            remove_btn.style.display = 'none';
-        }
-    }
-    function deletebooksearchtext() {
-        document.querySelector('form#search_book_form input[name=keyword]').value = '';
-        document.querySelector('form#search_book_form .remove_keyword').style.display = 'none';
-    }
-    function booksearch(page=1) {
-        if(fetch) return false;
-        fetch = true;
-        // form 데이터 처리
-        let form_data = new FormData(search_book_form);
-        form_data.append('page', page);
-        let url = search_book_form.action;
-        let shtml = '';
-        common.ax_fetch_put(url, form_data, function(res) {
-            fetch = false;
-            if(res.data.length > 0) {
-                if(page == 1) {
-                    book_data = [];
-                    document.querySelector('.book_search_list').innerHTML = '';
-                    shtml += '<li class="book_info">검색 결과 ( ' + res.info.total_count + ' )</li>';
-                }
-                book_data = book_data.concat(res.data);
-                res.data.forEach(function(v, k) {
-                    shtml += `<li class="book_info">
-                        <div class="book_image">
-                            <img src="${v.thumbnail}" alt="">
-                        </div>
-                        <div class="book_subinfo">
-                            <div class="book_title">${v.title}</div>
-                            <div class="book_isbn">ISBN : <span>${v.isbn13}</span></div>
-                            <div class="book_author">저자 : <span>${v.authors}</span></div>
-                            <div class="book_translator">번역 : <span>${v.translators}</span></div>
-                            <div class="book_publisher">출판 : <span>${v.publisher}</span></div>
-                            <div class="book_regdate">${v.regdate}</div>
-                            <div class="book_link"><a href="${v.link}" target="_blank">책 소개</a></div>
-                            <div class="book_button">
-                                <button class="add_book" onclick="Commonjs.add_bookshelf(${v.isbn13}, this)">우리 가족 책장에 담기 +</button>
-                            </div>
-                        </div>
-                    </li>`;
-                });
-                document.querySelector('.book_search_nodata').style.display = 'none';
-                page++;
-                if(!res.info.is_end) shtml += `<li class="book_info more" onclick="Commonjs.booksearch(${page})">더 보기 +</li>`;
-                if(document.querySelector('.book_info.more')) document.querySelector('.book_info.more').remove();
-                document.querySelector('.book_search_list').insertAdjacentHTML('beforeend', shtml);
-                if(document.querySelector('input[name=keyword]:focus')) document.querySelector(':focus').blur();
-            } else {
-                document.querySelector('.book_search_list').insertAdjacentHTML('beforeend', shtml);
-            }
-        });
-    }
-    function add_bookshelf(isbn13, btn) {
-        if(fetch) return false;
-        fetch = true;
-        let book = book_data.find(function(v) { if(v.isbn13 == isbn13) return true; });
-        let url = '/bookshelf';
-        let form_data = new FormData();
-        form_data.append('book', JSON.stringify(book));
-        common.ax_fetch_post(url, form_data, function(res) {
-            fetch = false;
-            if(res.success) {
-                common.notification('우리 가족 책장에 담았습니다.', 20);
-                btn.innerHTML = '우리 가족 책장에 담았습니다.';
-                btn.onclick = null;
-            } else {
-                if(res.code == 'already') {
-                    btn.innerHTML = res.message;
-                    btn.onclick = null;
-                }
-                if(res.message) alert(res.message);
-                if(res.code == 'logout') common.logout();
-                if(res.code == 'choose') common.choose();
-            }
-        });
-    }
     return {
         init: function() {
             scrolling();
         }(),
-        booksearchtext: booksearchtext,
-        deletebooksearchtext: deletebooksearchtext,
-        booksearch: booksearch,
-        add_bookshelf: add_bookshelf,
     }
 }();
